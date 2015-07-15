7c9453cc4d596364880af35b245fad0b0ec3239c
Felipe Martín <fmartingr@gmail.com>
2015-07-15 15:20:55 +0200

    Using list comprehensions

--- src/converters.py
+++ src/converters.py
@@ -3,7 +3,6 @@
 # core
 from games import game_runtime
 from guilds import guild_runtime
-from guilds import guild_services
 
 # back
 from dummy import DUMMY_LAST_TURN
@@ -11,13 +10,6 @@ from dummy import DUMMY_LAST_TURN
 
 # == game ==
 def convert_game(game: game_runtime.Game, guild: guild_runtime.Guild) -> object:
-    result = {}
-
-    result['places'] = _convert_places(game)
-    result['guild'] = _convert_guild(guild)
-    result['free_actions'] = _convert_actions(game.definition.free_actions)
-    result['last_turn'] = DUMMY_LAST_TURN
-
     return {
         'places': _convert_places(game),
         'guild': _convert_guild(guild),
@@ -28,32 +20,23 @@ def convert_game(game: game_runtime.Game, guild: guild_runtime.Guild) -> object:
 
 # == Places and actions ==
 def _convert_places(game: game_runtime.Game) -> object:
-    places = []
-
-    for place in game.places:
-        current = {
-            'name': place.definition.name,
-            'slug': place.definition.slug,
-            'actions': _convert_actions(place.definition.actions),
-        }
-
-        places.append(current)
+    places = [{
+        'name': place.definition.name,
+        'slug': place.definition.slug,
+        'actions': _convert_actions(place.definition.actions),
+    } for place in game.places]
 
     return places
 
 
 def _convert_actions(actions) -> object:
-    results = []
-
-    for action in actions:
-        current = {
-            'slug': action.slug,
-            'name': action.name,
-            'action_points': action.action_points,
-            'skills_needed': action.skills_needed,
-            'skills_upgraded': action.skills_needed,
-        }
-        results.append(current)
+    results = [{
+        'slug': action.slug,
+        'name': action.name,
+        'action_points': action.action_points,
+        'skills_needed': action.skills_needed,
+        'skills_upgraded': action.skills_needed,
+    } for action in actions]
 
     return results
 
@@ -70,45 +53,35 @@ def _convert_guild(guild: guild_runtime.Guild) -> object:
 
 
 def _convert_assets(guild: guild_runtime.Guild) -> object:
-    assets = []
-    for asset in guild.assets:
-        assets.append({
-            'name': asset.name,
-            'slug': asset.slug,
-            'value': asset.value,
-        })
+    assets = [{
+        'name': asset.name,
+        'slug': asset.slug,
+        'value': asset.value,
+    } for asset in guild.assets]
+
     return assets
 
 
 def _convert_members(guild: guild_runtime.Guild) -> object:
-    members = []
-    for member in guild.members:
-        current = {
-            'name': member.name,
-            'slug': member.slug,
-            'archetype': member.archetype,
-            'archetype_slug': member.archetype_slug,
-            'skills': [],
-            'conditions': [],
-        }
+    members = [{
+        'name': member.name,
+        'slug': member.slug,
+        'archetype': member.archetype,
+        'archetype_slug': member.archetype_slug,
+        'skills': [{
+            'name': skill.name,
+            'slug': skill.slug,
+            'value': skill.value,
+            'modifier': skill.modifier,
+        } for skill in member.skills],
+        'conditions': [{
+            'name': condition.name,
+            'slug': condition.slug,
+            'type': condition.type,
+            'description': condition.description,
+        } for condition in member.conditions],
+    } for member in guild.members]
 
-        for skill in member.skills:
-            current['skills'].append({
-                'name': skill.name,
-                'slug': skill.slug,
-                'value': skill.value,
-                'modifier': skill.modifier,
-            })
-
-        for condition in member.conditions:
-            current['conditions'].append({
-                'name': condition.name,
-                'slug': condition.slug,
-                'type': condition.type,
-                'description': condition.description,
-            })
-
-        members.append(current)
     return members
 
 
